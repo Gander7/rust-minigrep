91fa5f80108bb10138889627e75e96293437133e
Zach Allen <zach@basebridge.com>
2019-10-31 15:24:41 -0300

    added search fn with test

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,24 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
     println!("With Text:\n{}", content);
 
+    println!("Results:");
+    for line in search(&config.query, &content) {
+        println!("{}", line);
+    }
+
     Ok(())
+}
+
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
 
 pub struct Config {
@@ -22,5 +39,24 @@ impl Config {
         let filename = args[2].clone();
 
         Ok(Config { query, filename })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        );
     }
 }
