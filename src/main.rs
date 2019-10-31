37a3259229ce1fd9475f8601264924ce52710823
Zach Allen <zach@basebridge.com>
2019-10-31 14:33:33 -0300

    added reading from file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In File: {}", filename);
+
+    let content = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    println!("With Text:\n{}", content);
 }
